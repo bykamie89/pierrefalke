b7286939cd95096375eee923a8fb7cbccdf0ae87
Kamilla <kamillakkngmail.com>
2023-02-23 21:24:50 +0100

    changes ti 1+2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import "./style.css";
+import './style.css';
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from 'motion';
+
+inView('.textinviewleft', () => {
+  animate('.textinviewleft', { x: [-2000, 0] }, { duration: 2 });
+});
+
+inView('.textinviewright', () => {
+  animate('.textinviewright', { x: [2000, 0] }, { duration: 2 });
+});
